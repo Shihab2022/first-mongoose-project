772b36dbdb00c1a05a54370da120c1b3df300852
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2023-12-05 16:09:44 +0600

    add transaction in delete student

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,4 +1,8 @@
+import mongoose from "mongoose";
 import { Student } from "./student.model";
+import AppError from "../../errors/AppError";
+import httpStatus from "http-status";
+import { User } from "../user/user.model";
 
 
 const getAllStudentFromDB = async () => {
@@ -21,8 +25,28 @@ const getSingleStudentFromDB = async (id: string) => {
     return result
 }
 const deleteStudentFromDB = async (id: string) => {
-    const result = await Student.updateOne({ id }, { isDeleted: true })
-    return result
+
+    const session = await mongoose.startSession()
+    try {
+        session.startTransaction()
+
+        const deleteStudent = await Student.findByIdAndUpdate({ id }, { isDeleted: true }, { new: true, session })
+        if (!deleteStudent) {
+            throw new AppError(httpStatus.BAD_REQUEST, "Failed to delete student")
+        }
+
+        const deleteUser = await User.findByIdAndUpdate({ id }, { isDeleted: true }, { new: true, session })
+        if (!deleteUser) {
+            throw new AppError(httpStatus.BAD_REQUEST, "Failed to delete user")
+        }
+        await session.commitTransaction()
+        await session.endSession()
+        return deleteStudent
+    } catch (error) {
+        await session.abortTransaction()
+        await session.endSession()
+    }
+
 }
 export const studentServices = {
     getAllStudentFromDB,
