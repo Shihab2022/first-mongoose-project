836aee006dbb951feebc9012dfda2e61f3a11ba5
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2023-12-06 08:52:57 +0600

    handleDuplicateError

--- src/app/middlewares/globalErrorHandllers.ts
+++ src/app/middlewares/globalErrorHandllers.ts
@@ -8,6 +8,8 @@ import { ZodError, ZodIssue } from 'zod';
 import config from '../config';
 import handleZodError from '../errors/handleZodError';
 import handleValidationError from '../errors/handleValidationError';
+import handleCastError from '../errors/handleCastError';
+import handleDuplicateError from '../errors/handleDuplicateError';
 
 
 const globalErrorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
@@ -15,7 +17,7 @@ const globalErrorHandler = (error: any, req: Request, res: Response, next: NextF
     let statusCode = error.statusCode || 500;
     let message = error.message || 'Something went wrong !'
 
-    let errorSources: TErrorSource[] = [
+    let errorSources: TErrorSource = [
         {
             path: '',
             message: 'Something went wrong'
@@ -28,13 +30,27 @@ const globalErrorHandler = (error: any, req: Request, res: Response, next: NextF
         const simplifiedError = handleZodError(error)
         statusCode = simplifiedError.statusCode;
         message = simplifiedError.message;
-        errorSources = simplifiedError.errorSource
+        errorSources = simplifiedError.errorSources
 
     }
 
     else if (error.name === "ValidationError") {
 
         const simplifiedError = handleValidationError(error)
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+        errorSources = simplifiedError.errorSources
+    }
+    else if (error.name === "CastError") {
+
+        const simplifiedError = handleCastError(error)
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+        errorSources = simplifiedError.errorSources
+    }
+    else if (error.code === 11000) {
+
+        const simplifiedError = handleDuplicateError(error)
         statusCode = simplifiedError.statusCode;
         message = simplifiedError.message;
         errorSources = simplifiedError.errorSources
