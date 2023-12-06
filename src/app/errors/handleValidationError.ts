f061346e32ecf411c13998301b38e58112f3aefc
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2023-12-06 08:33:08 +0600

    add type in error return

--- src/app/errors/handleValidationError.ts
+++ src/app/errors/handleValidationError.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
-import { TErrorSource } from "../interface/error";
+import { TErrorSource, TGenericErrorResponse } from "../interface/error";
 
-const handleValidationError = (error: mongoose.Error.ValidationError) => {
+
+const handleValidationError = (error: mongoose.Error.ValidationError): TGenericErrorResponse => {
     // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-    const errorSources: TErrorSource[] = Object.values(error.errors).map((value: mongoose.Error.ValidationError | mongoose.Error.CastError | mongoose.Error) => {
+    const errorSources: TErrorSource = Object.values(error.errors).map((value: mongoose.Error.ValidationError | mongoose.Error.CastError | mongoose.Error) => {
         return {
             path: value?.path,
             message: value.message,
--- src/app/errors/handleZodError.ts
+++ src/app/errors/handleZodError.ts
@@ -1,9 +1,9 @@
 import { ZodError, ZodIssue } from "zod"
-import { TErrorSource } from "../interface/error"
+import { TErrorSource, TGenericErrorResponse } from "../interface/error"
 
-const handleZodError = (err: ZodError) => {
+const handleZodError = (err: ZodError): TGenericErrorResponse => {
 
-    const errorSource: TErrorSource[] = err.issues.map((issue: ZodIssue) => {
+    const errorSources: TErrorSource = err.issues.map((issue: ZodIssue) => {
         return {
             path: issue?.path[issue.path.length - 1],
             message: issue.message
@@ -13,7 +13,7 @@ const handleZodError = (err: ZodError) => {
     return {
         statusCode,
         message: "validation Error",
-        errorSource
+        errorSources
     }
 }
 
