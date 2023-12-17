5a680fe6c0ae9b31fa341d3b01cd919daae01df8
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2023-12-17 17:12:24 +0600

    add validation for start time and end time

--- src/app/modules/offeredCourse/offeredCourse.validation.ts
+++ src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -14,8 +14,27 @@ const createOfferedCourseValidation = z.object({
         maxCapacity: z.number(),
         section: z.number(),
         days: z.array(z.enum([...DaysEnum] as [string, ...string[]])),
-        startTime: z.string(),
-        endTime: z.string(),
+        startTime: z.string().refine((time) => {
+            const regex = /^[01]?[0-9]|2[0-3]:[0-5][0-9]/ ///00-09 10-19 20-23
+            return regex.test(time)
+        }, {
+            message: "Invalid time formate , expected 'HH:MM' in 24 hours formate"
+        }),
+        endTime: z.string().refine((time) => {
+            const regex = /^[01]?[0-9]|2[0-3]:[0-5][0-9]/ ///00-09 10-19 20-23
+            return regex.test(time)
+        }, {
+            message: "Invalid time formate , expected 'HH:MM' in 24 hours formate"
+        }),
+    }).refine(({ body }) => {
+        // for compare time we let a random time for campare
+        // start time : 10:30 ===> 2000-01-01T10:30
+        // end time : 12:30 ===> 2000-01-01T12:30
+        const start = new Date(`2000-01-01T${body.startTime}`)
+        const end = new Date(`2000-01-01T${body.endTime}`)
+        return end > start
+    }, {
+        message: "Start time should be before end time"
     })
 })
 const updateOfferedCourseValidation = z.object({
@@ -23,8 +42,18 @@ const updateOfferedCourseValidation = z.object({
         faculty: z.string().optional(),
         maxCapacity: z.number().optional(),
         days: z.enum([...DaysEnum] as [string, ...string[]]).optional(),
-        startTime: z.string().optional(),
-        endTime: z.string().optional(),
+        startTime: z.string().refine((time) => {
+            const regex = /^[01]?[0-9]|2[0-3]:[0-5][0-9]/ ///00-09 10-19 20-23
+            return regex.test(time)
+        }, {
+            message: "Invalid time formate , expected 'HH:MM' in 24 hours formate"
+        }).optional(),
+        endTime: z.string().refine((time) => {
+            const regex = /^[01]?[0-9]|2[0-3]:[0-5][0-9]/ ///00-09 10-19 20-23
+            return regex.test(time)
+        }, {
+            message: "Invalid time formate , expected 'HH:MM' in 24 hours formate"
+        }).optional(),
     })
 })
 
