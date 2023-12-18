6f981da469d467c564db1565311cd6ef35073257
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2023-12-18 15:09:56 +0600

    verify access token

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -2,16 +2,29 @@
 import { NextFunction, Request, Response } from 'express'
 import AppError from '../errors/AppError'
 import httpStatus from 'http-status'
+import jwt, { JwtPayload } from "jsonwebtoken"
+import config from '../config'
 
 
 const auth = () => {
     return async (req: Request, res: Response, next: NextFunction) => {
         try {
             const token = req.headers.authorization
+            // check user send token or not
             if (!token) {
                 throw new AppError(httpStatus.UNAUTHORIZED, "Your are not authorized !!")
             }
-            next()
+
+            /// verify the token
+            jwt.verify(token, config.jwt_access_secret as string, function (err, decoded) {
+                if (err) {
+                    throw new AppError(httpStatus.UNAUTHORIZED, "Your are not authorized !!")
+                }
+
+                req.user = decoded as JwtPayload
+                next()
+            })
+
         } catch (error) {
             next(error)
         }
